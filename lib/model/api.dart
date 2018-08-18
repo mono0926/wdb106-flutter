8b7c519368075a96ca86e8a214b0f072806bdda2
Masayuki Ono <mono0926@gmail.com>
2018-08-19 07:46:17 +0900

    Refactor

--- lib/model/api.dart
+++ lib/model/api.dart
@@ -14,9 +14,9 @@ class MockyApiClient implements ApiClient {
   Future<List<Item>> getItems() async {
     final result =
         await client.get('http://www.mocky.io/v2/5b7812292e00005200864c0d');
-    final json = (await jsonDecode(result.body)) as List<dynamic>;
-    final list =
-        json.map((j) => Item.fromJSON(j as Map<String, dynamic>)).toList();
+    final json =
+        ((await jsonDecode(result.body)) as List).cast<Map<String, dynamic>>();
+    final list = json.map((j) => Item.fromJSON(j)).toList();
     return list;
   }
 }
