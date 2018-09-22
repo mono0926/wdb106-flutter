cc04bcc5e6c46359e6bc94db481b4df269d9048f
Masayuki Ono <mono0926@gmail.com>
2018-09-22 21:10:53 +0900

    Use image_test_utils
    - https://pub.dartlang.org/packages/image_test_utils
    - https://medium.com/flutter-community/quick-tip-2-why-does-using-image-network-crash-widget-tests-fc566430da01

--- test/smoke_test.dart
+++ test/smoke_test.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:image_test_utils/image_test_utils.dart';
 import 'package:wdb106_sample/bloc/items_bloc.dart';
 
 import 'package:wdb106_sample/main.dart';
@@ -10,14 +11,8 @@ import 'package:mockito/mockito.dart';
 import 'helper/api.dart';
 
 void main() {
-  // See: https://github.com/flutter/flutter/blob/master/packages/flutter/test/widgets/image_headers_test.dart
-  final MockHttpClient client = MockHttpClient();
-  final MockHttpClientRequest request = MockHttpClientRequest();
-  final MockHttpClientResponse response = MockHttpClientResponse();
-  final MockHttpHeaders headers = MockHttpHeaders();
-
   testWidgets('Smoke test', (tester) async {
-    await HttpOverrides.runZoned(() async {
+    await provideMockedNetworkImages(() async {
       await tester.pumpWidget(App(
         itemsBloc: ItemsBloc(
           client: MockApiClient(),
@@ -65,104 +60,6 @@ void main() {
 
       // カート screen closed automatically by cart cleared
       expect(find.text('商品リスト'), findsOneWidget);
-    }, createHttpClient: (_) {
-      // ignore: argument_type_not_assignable
-      when(client.getUrl(typed(any)))
-          .thenAnswer((_) => Future<HttpClientRequest>.value(request));
-      when(request.headers).thenReturn(headers);
-      when(request.close())
-          .thenAnswer((_) => Future<HttpClientResponse>.value(response));
-      when(response.contentLength).thenReturn(kTransparentImage.length);
-      when(response.statusCode).thenReturn(HttpStatus.ok);
-      // ignore: argument_type_not_assignable
-      when(response.listen(typed(any))).thenAnswer((invocation) {
-        final void Function(List<int>) onData =
-            invocation.positionalArguments[0];
-        final void Function() onDone = invocation.namedArguments[#onDone];
-        final void Function(Object, [StackTrace]) onError =
-            invocation.namedArguments[#onError];
-        final bool cancelOnError = invocation.namedArguments[#cancelOnError];
-        return new Stream<List<int>>.fromIterable(
-                <List<int>>[kTransparentImage])
-            .listen(onData,
-                onDone: onDone, onError: onError, cancelOnError: cancelOnError);
-      });
-      return client;
     });
   });
 }
-
-class MockHttpClient extends Mock implements HttpClient {}
-
-class MockHttpClientRequest extends Mock implements HttpClientRequest {}
-
-class MockHttpClientResponse extends Mock implements HttpClientResponse {}
-
-class MockHttpHeaders extends Mock implements HttpHeaders {}
-
-const List<int> kTransparentImage = const <int>[
-  0x89,
-  0x50,
-  0x4E,
-  0x47,
-  0x0D,
-  0x0A,
-  0x1A,
-  0x0A,
-  0x00,
-  0x00,
-  0x00,
-  0x0D,
-  0x49,
-  0x48,
-  0x44,
-  0x52,
-  0x00,
-  0x00,
-  0x00,
-  0x01,
-  0x00,
-  0x00,
-  0x00,
-  0x01,
-  0x08,
-  0x06,
-  0x00,
-  0x00,
-  0x00,
-  0x1F,
-  0x15,
-  0xC4,
-  0x89,
-  0x00,
-  0x00,
-  0x00,
-  0x0A,
-  0x49,
-  0x44,
-  0x41,
-  0x54,
-  0x78,
-  0x9C,
-  0x63,
-  0x00,
-  0x01,
-  0x00,
-  0x00,
-  0x05,
-  0x00,
-  0x01,
-  0x0D,
-  0x0A,
-  0x2D,
-  0xB4,
-  0x00,
-  0x00,
-  0x00,
-  0x00,
-  0x49,
-  0x45,
-  0x4E,
-  0x44,
-  0xAE,
-];
