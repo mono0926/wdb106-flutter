1da05d6563b17369b42cc4a2379f0a277dbc74c0
Masayuki Ono <mono0926@gmail.com>
2020-03-09 12:24:08 +0900

    Refactor

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      key: context.watch<GlobalKey<NavigatorState>>(),
+      navigatorKey: context.watch(),
       home: const ItemsPage(),
     );
   }
