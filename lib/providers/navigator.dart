d3f19fa17fe1e1c08e1d2c539624d6230bc83ea1
Masayuki Ono <mono0926@gmail.com>
2021-08-19 09:33:11 +0900

    Delete unused code

--- lib/providers/navigator.dart
+++ lib/providers/navigator.dart
@@ -2,7 +2,3 @@ import 'package:flutter/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final navigatorKeyProvider = Provider((_) => GlobalKey<NavigatorState>());
-
-extension NavigatorStateEx on NavigatorState {
-  BuildContext get descendantContext => overlay!.context;
-}
